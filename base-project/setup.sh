a3210dfa7a97987ddddabba3840ef557303d64cc
mdamour1976 <mdamour1976@gmail.com>
2013-12-17 20:52:18 -0500

    Updated pom

---
+++ base-project/setup.sh
@@ -0,0 +1,2 @@
+mvn install:install-file -Dfile=./war/WEB-INF/lib/ColorPicker-GWT-2.1.jar -DgroupId=net.auroris -DartifactId=gwt-colorpicker -Dversion=2.1 -Dpackaging=jar
+mvn install:install-file -Dfile=./war/WEB-INF/lib/captcha-at.jar -DgroupId=com.andrewtimberlake -DartifactId=captcha -Dversion=1.0 -Dpackaging=jar
